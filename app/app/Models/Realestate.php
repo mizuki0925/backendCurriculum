1611e983c899e9fe721f0c6e79405cbaaae4ea09
mizuki <km0925q@gmail.com>
2022-04-20 21:47:19 +0900

    リレーションの設定（未確認）

--- app/app/Models/Realestate.php
+++ app/app/Models/Realestate.php
@@ -10,4 +10,9 @@ class Realestate extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function accounts()
+    {
+        return $this->belongsTo(Account::class);
+    }
 }
--- app/app/Models/account.php
+++ app/app/Models/account.php
@@ -10,4 +10,9 @@ class Account extends Model
 {
     use HasFactory;
     use SoftDeletes;
+
+    public function realestates()
+    {
+        return $this->hasMany(Realestate::class);
+    }
 }
