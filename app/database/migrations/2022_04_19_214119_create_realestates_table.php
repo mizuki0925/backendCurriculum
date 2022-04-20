28df92e51efc224b976e60380e71d91f10efbed3
mizuki <km0925q@gmail.com>
2022-04-20 21:47:46 +0900

    migrateファイルの修正

--- app/database/migrations/2022_04_19_214119_create_realestates_table.php
+++ app/database/migrations/2022_04_19_214119_create_realestates_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
             $table->string('adress', 255)->comment('住所');
             $table->unsignedInteger('breadth')->nullable()->comment('広さ');
             $table->string('floor_plan')->nullable()->comment('間取り');
-            $table->unsignedInteger('tenancy_status')->unique()->comment('入居状況');
+            $table->unsignedInteger('tenancy_status')->comment('入居状況');
             $table->unsignedBigInteger('user_id')->comment('物件登録者');
             $table->foreign('user_id')->references('id')->on('accounts');
             $table->timestamps();
